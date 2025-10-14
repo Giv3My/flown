5544ca718c4b07046eed29bd54a97d3dfa0161d6
Vladislav Bessmertnykh <81647268+Giv3My@users.noreply.github.com>
2025-10-14 15:50:29 +0300

    feat(react-router): Implement app routing (#1)

---
+++ src/constants/routes.ts
@@ -0,0 +1,20 @@
+export const AUTH_ROUTES = {
+  LOGIN: '/auth/login',
+  REGISTER: '/auth/register',
+  REGISTER_CREDENTIALS: '/auth/register/credentials',
+};
+
+export const AUTH_ROUTES_ARRAY = Object.values(AUTH_ROUTES);
+
+export const PROTECTED_ROUTES = {
+  HOME: '/',
+};
+
+export const PROTECTED_ROUTES_ARRAY = Object.values(PROTECTED_ROUTES);
+
+export const PUBLIC_ROUTES = {
+  NOT_FOUND: '/404',
+  ANY: '*',
+};
+
+export const PUBLIC_ROUTES_ARRAY = Object.values(PUBLIC_ROUTES);
---
+++ src/pages/main/index.ts
@@ -0,0 +1,3 @@
+export * from './main-layout';
+export * from './home';
+export * from './not-found';
