219d50abf0ea1299cac0169b3ecee4b8a4aee32b
Vladislav Bessmertnykh <81647268+Giv3My@users.noreply.github.com>
2025-10-24 13:40:32 +0300

    fix(use-sort): Add strategies to useSort hook (#8)

--- src/hooks/use-sort/types.ts
+++ src/hooks/use-sort/types.ts
@@ -1,3 +1,10 @@
+export type Comparator<T = unknown> = (a: T, b: T) => number
+
+export interface StrategyItem {
+  when: (a: unknown, b: unknown) => boolean
+  compare: Comparator
+}
+
 export const isDateLike = (v: unknown): v is Date | string => {
   return v instanceof Date || (typeof v === 'string' && !isNaN(Date.parse(v)))
 }
